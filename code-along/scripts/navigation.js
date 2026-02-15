02fad0947706ac849e925cabe8d5da8a66e07327
dungasamsungo <sampsonngobi@gmail.com>
2026-02-15 19:54:02 -0300

    Code along for header and footer completed

--- code-along/scripts/navigation.js
+++ code-along/scripts/navigation.js
@@ -1,5 +1,8 @@
-const navButton = document.querySelector('#ham-btn');
+const navButton = document.querySelector('#ham-btn'); //select the navigation button
+const navbar = document.querySelector('#nav-bar'); //Select the navigation bar
+
 
 navButton.addEventListener('click', () =>{
     navButton.classList.toggle('show');
+    navbar.classList.toggle('show');
 } )
